3cb9e0a6ad043813cfa4ff518636522a7d1b665d
jep <n.siomusic@gmail.com>
2024-04-17 12:46:32 +0700

    [RED] Add tests for Product model.

---
+++ src/test/java/id/ac/ui/cs/advprog/productservice/model/ProductTest.java
@@ -0,0 +1,76 @@
+package id.ac.ui.cs.advprog.productservice.model;
+
+import id.ac.ui.cs.advprog.productservice.ProductServiceApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//ID dalam format UUID
+//Nama produk
+//Jenis produk (tag produk, seperti ini)
+//Deskripsi
+//Gambar
+//Harga asli
+//Harga setelah diskon, jika ada
+
+
+public class ProductTest {
+    Product product;
+    Set<Tag> productTags = new HashSet<>();
+
+    @BeforeEach
+    void setUp() {
+        this.product = Product.builder()
+                .name("Kursi Kayu")
+                .tag(new Tag("Wooden"))
+                .tag(new Tag("Chair"))
+                .description("Produk ini adalah kursi kayu sederhana yang cocok untuk ruangan mana saja.")
+                .image("https://i.ibb.co/KzQjH3p/sam-moghadam-khamseh-kvmds-Tr-GOBM-unsplash.jpg")
+                .price(300000)
+                .discountPrice(270000)
+                .build();
+    }
+
+    @Test
+    void testIdExist() {
+        assertNotNull(this.product.getId());
+    }
+    @Test
+    void testChangeId() {
+        this.product.setId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getId().toString());
+    }
+    @Test
+    void testGetName() {
+        assertEquals(this.product.getName(), "Kursi Kayu");
+    }
+    @Test
+    void testGetTags() {
+        this.productTags.add(new Tag("Wooden"));
+        this.productTags.add(new Tag("Chair"));
+
+        assertEquals(this.product.getTag(), this.productTags);
+    }
+    @Test
+    void testGetDescription() {
+        assertEquals(this.product.getDescription(),
+                "Produk ini adalah kursi kayu sederhana yang cocok untuk ruangan mana saja.");
+    }
+    @Test
+    void testGetImage() {
+        assertEquals(this.product.getImage(),
+                "https://i.ibb.co/KzQjH3p/sam-moghadam-khamseh-kvmds-Tr-GOBM-unsplash.jpg");
+    }
+    @Test
+    void testGetPrice() {
+        assertEquals(this.product.getPrice(), 300000);
+    }
+    @Test
+    void testGetDiscountPrice() {
+        assertEquals(this.product.getDiscountPrice(), 270000);
+    }
+}
