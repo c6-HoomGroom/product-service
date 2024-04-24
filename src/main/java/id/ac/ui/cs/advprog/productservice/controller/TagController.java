1ec66a81c4adc2a28d5faabf0b38384b595e8192
jep <n.siomusic@gmail.com>
2024-04-24 18:51:11 +0700

    Implement Controller and Pages for Tag

---
+++ src/main/java/id/ac/ui/cs/advprog/productservice/controller/TagController.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.productservice.controller;
+
+import id.ac.ui.cs.advprog.productservice.model.Tag;
+import id.ac.ui.cs.advprog.productservice.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/tags")
+public class TagController {
+    @Autowired
+    private TagService tagService;
+
+    @GetMapping("/add")
+    public String createTagPage(Model model) {
+        Tag tag = new Tag();
+        model.addAttribute("tag", tag);
+        return "tagCreate";
+    }
+
+    @PostMapping("")
+    public String createTagPost(@ModelAttribute Tag tag, Model model) {
+        tagService.create(tag);
+        return "redirect:/tags";
+    }
+
+    @GetMapping("")
+    public String tagListPage(Model model) {
+        List<Tag> tags = tagService.findAll();
+        model.addAttribute("tags", tags);
+        return "tagList";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCar(@RequestParam("tagId") String tagId) {
+        tagService.delete(tagId);
+        return "redirect:/tags";
+    }
+}
